44b5679072dec54330d4b8c261e9573eb7ca9dba
evertcolombia <evertescalante@gmail.com>
2020-01-31 20:43:49 +0000

    hotfix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,42 @@
-#
+#include "sort.h"
+#include <stdio.h>
+
+int *do_swap(int *array, int pos1, int pos2);
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+
+	if (size < 2)
+		return;
+
+	for (i = 1; i <= size; i++)
+	{
+		if (array[i - 1] > array[i])
+		{
+			if (i == size)
+			{
+				do_swap(array, i - 1, i);
+				break;
+			}
+			else
+			{
+				do_swap(array, i - 1, i);
+				break;
+			}
+		}
+		if (i == size)
+			size -= 2;
+	}
+}
+
+int *do_swap(int *array, int pos1, int pos2)
+{
+	int key;
+
+	key  = array[pos1];
+	array[pos1 ]= array[pos2];
+	array[pos2] = key;
+	return (array);
+
+}
