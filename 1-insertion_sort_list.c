edb9ab444ae79592cca588fd4d7ea3626f776058
evertcolombia <evertescalante@gmail.com>
2020-02-01 22:12:33 +0000

    hotfix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,30 +1,46 @@
 #include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void change_no(listint_t *, listint_t *a, listint_t *b);
 
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp = NULL, *a = NULL, *b = NULL;
 
 	if (!*list)
-		return (NULL);
+		return;
 
-	tmp = *list->next;
+	tmp = *list;
+	tmp = tmp->next;
 
 	while (tmp)
 	{
-		if (tmp->prev->value > tmp->value)
-		{
-			a = tmp->prev;
+		a = tmp->prev;
+		if (tmp->next)
 			b = tmp->next;
 
-			if (b)
-				b->prev = a, a->next = b;
-			
-			a->prev = tmp;
-			tmp->prev = a->prev;
-			tmp->next = a;
-
-			/*si hay algo en tmp->prev->prev
-			que en su next apunte a tmp*/
-		}
+		if (a->n > tmp->n)
+		{
+			change_no(tmp, a, b);
+			print_list(*list);
+		} tmp = tmp->next;
 	}
+}
+
+void change_no(listint_t *t, listint_t *a, listint_t *b)
+{
+
+	a->next = b;
+	b->prev = a;
+
+	/*if (t->next)
+		t->next = a;*/
+
+	if (a->prev)
+	{
+		a->prev->next = t;
+		a->prev = t;
+	}
+	a->prev = t;
 }
