7c964180e243cf3ff017dfb8e2c4cdb0eeb90b84
evertcolombia <evertescalante@gmail.com>
2020-02-04 16:53:27 +0000

    bubble sort commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,32 +5,20 @@ int *do_swap(int *array, int pos1, int pos2);
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
+	size_t i, j;
 
 	if (size < 2)
 		return;
 
-	for (i = 1; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-
-		while (i < size)
+		for (j = i; j < size - 1; j++)
 		{
-			if (array[i - 1] > array[i])
+			if (array[j] > array[j + 1])
 			{
-				if (i == size)
-				{
-					do_swap(array, i - 1, i);
-	                                print_array(array, size);
-					i++;
-				}
-				else
-				{
-					do_swap(array, i - 1, i);
-	        	               	print_array(array, size);
-					i++;
-				}
+				do_swap(array, j, j + 1);
+				print_array(array, size);
 			}
-			i++;
 		}
 	}
 }
