cbd73120c32ada69c6683e6be8079d8a51170630
evertcolombia <evertescalante@gmail.com>
2020-02-05 17:20:01 +0000

    hotfix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,22 +7,22 @@ int *do_swap(int *array, int current, int tmp);
 void selection_sort(int *array, size_t size)
 {
 	unsigned int j, c = 0;
-	int current, flag = 0;
+	int current, flag, tmp;
 	
-	while(c < size && size > 1)
+	while(c < size)
 	{
-		current = c, flag = 0;
+		current = c, flag = 0, tmp = c;
 
-		for (j = c + 1; j < size; j++)
+		for (j= c + 1; j < size; j++)
 		{
 			if (array[j] < array[current])
 				current = j, flag = 1;
-		}
+		} c++;
 		if (flag)
 		{
-			do_swap(array, current, j);
+			do_swap(array, current, tmp);
 			print_array(array, size);
-		} c++;
+		}
 	}
 }
 
